1c6e6683c917d7d83f1ca02f154439cfa923c986
gabrielbmotta <gabrielbenmotta@gmail.com>
2021-09-30 16:31:16 -0400

    fix

--- src/commandwatcher.cpp
+++ src/commandwatcher.cpp
@@ -1,4 +1,5 @@
 #include "commandwatcher.hpp"
+#include <iostream>
 
 CommandWatcher::CommandWatcher()
 : m_callbackCount(0)
@@ -18,19 +19,26 @@ void CommandWatcher::disconnect()
 
 void CommandWatcher::registerCallback(std::string str, void (*func)(std::string))
 {
-    m_callbackMap.insert(m_callbackCount, Callback(str, func));
-
+    m_callbackMap.insert(std::pair<int, Callback>(m_callbackCount, Callback(str, func)));
     ++m_callbackCount;
 }
 
 void CommandWatcher::deleteCallback(std::string, void (*func)(std::string))
 {
-
+    
 }
 
 void CommandWatcher::deleteCallback(int)
 {
 
+}
+
+void CommandWatcher::showCallbacks()
+{
+    for (int i = 0; i < m_callbacks.size(); i++)
+    {
+        std::cout << "(" << i << ") - '" << m_callbackList;
+    }
 }
 
 void CommandWatcher::startWatching()
