73cf0117ba7307b03d2cdd945f251509d6ae4a85
juangpc <juangpc@gmail.com>
2021-09-30 17:51:25 -0400

    minor edit

--- src/controller.cpp
+++ src/controller.cpp
@@ -12,7 +12,7 @@ void Controller::start()
 {
     if(mIsActive)
     {
-        std::cout << "Hello. Everything is working fine. Move along!\n";
+        std::cout << "Everything is working fine. Move along!\n";
     } else {
         std::cout << "I'm actually going to do some work here.\n";
         mIsActive = true;
