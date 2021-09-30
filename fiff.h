5a2f0362ec372291dc48ace868501c5c2434b098
Gabriel B Motta <gbmotta@mgh.harvard.edu>
2021-09-30 11:26:27 -0400

    Add fiff header and skeleton of classes and functions

---
+++ fiff.h
@@ -0,0 +1,6 @@
+//Block Tags
+#define FIFF_BLOCK_START    104
+#define FIFF_BLOCK_END      105
+
+//Block Types
+#define FIFFB_RAW_DATA      102
--- main.cpp
+++ main.cpp
@@ -1,4 +1,61 @@
+#include "fiff.h"
+#include <iostream>
+
+struct Tag
+{
+    int kind;
+    int type;
+    int size;
+    int next;
+    char* data;
+};
+
+
+struct Measurement
+{
+    float   sampleFrequency;
+    int     numChannels;
+};
+
+class ShMemWatcher
+{
+public:
+    ShMemWatcher();
+
+
+
+private:
+
+    void connectToSharedMemory()
+    {
+
+    }
+
+    void processTag(const Tag& tag)
+    {
+        switch (tag.kind)
+    }
+
+    Tag getTagFromShMem()
+    {
+
+    }
+
+    Mesurement* currentMeasurement;
+};
+
+
+
 int main (int argc, char* argv[])
 {
+    SharedMemWatcher watcher;
+    connectToSharedMemory();
+
+    bool keepReading = true;
+
+    while(keepReading){
+        auto tag = getTagFromShMem();
+        processTag(tag);
+    }
     return 0;
 }
