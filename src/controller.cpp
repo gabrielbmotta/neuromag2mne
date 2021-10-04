6f939efd58fe49f9d04372c16b104746fae748d0
gabrielbmotta <gabrielbenmotta@gmail.com>
2021-10-04 17:36:51 -0400

    example start and stop

--- src/controller.cpp
+++ src/controller.cpp
@@ -11,8 +11,6 @@ Controller::Controller()
 {
     m_commandWatcher->registerCallback("test", aaaaa);
     m_commandWatcher->startWatching();
-    sleep(5);
-    m_commandWatcher->stopWatching();
 }
 
 void Controller::start()
@@ -20,6 +18,12 @@ void Controller::start()
     if(mIsActive)
     {
         std::cout << "Everything is working fine. Move along!\n";
+        if(m_commandWatcher->isWatching()){
+            m_commandWatcher->stopWatching();
+        } else{
+            m_commandWatcher->startWatching();
+        }
+
     } else {
         std::cout << "I'm actually going to do some work here.\n";
         mIsActive = true;
