689da461d7d6b2d4348b9622da31e38861cd0925
juangpc <juangpc@gmail.com>
2021-10-18 19:23:50 -0400

    class templates in header

--- src/utils/SharedPointer.h
+++ src/utils/SharedPointer.h
@@ -5,23 +5,116 @@ template<typename T>
 class SharedPointer
 {
 public:
-    SharedPointer();
+    SharedPointer()
+        :_ptr{nullptr},_count{nullptr}
+    {
+    }
 
-    SharedPointer(T* ptr);
+    SharedPointer(T* ptr)
+        :_ptr{nullptr},_count{nullptr}
+    {
+        if ( ptr != nullptr )
+        {
+            _count = new unsigned int;
+            *_count = 1;
+            _ptr = ptr;
+        }
+    }
 
-    SharedPointer(const SharedPointer& other);
+    SharedPointer(const SharedPointer& other)
+        : _ptr{other._ptr}, _count{other._count}
+    {
+        if( other._ptr != nullptr )
+        {
+            ++(*_count);
+        }
+    }
 
-    SharedPointer(SharedPointer&& other);
+    SharedPointer(SharedPointer&& other)
+    {
+        if (other._ptr != nullptr)
+        {
+            _ptr = other._ptr;
+            _count = other._count;
+            other._ptr = nullptr;
+            other._count = nullptr;
+        }
+    }
 
-    SharedPointer& operator=(const SharedPointer& other);
+    SharedPointer& operator=(const SharedPointer& other)
+    {
+        if( this != &other )
+        {
+            if ( _ptr != nullptr )
+            {
+                (*_count)--;
+                if ( *_count == 0 )
+                {
+                    delete _count;
+                    _count = nullptr;
+                    delete _ptr;
+                    _ptr = nullptr;
+                }
+            }
+            _ptr = other._ptr;
+            _count = other._count;
+            if ( _count != nullptr)
+            {
+                (*_count)++;
+            }
+        }
+        return *this;
+    }
 
-    SharedPointer& operator=(SharedPointer&& other);
+    SharedPointer& operator=(SharedPointer&& other)
+    {
+        if( this != &other )
+        {
+            if ( _ptr != nullptr )
+            {
+                (*_count)--;
+                if ( *_count == 0 )
+                {
+                    delete _count;
+                    _count = nullptr;
+                    delete _ptr;
+                    _ptr = nullptr;
+                }
+            }
+            _ptr = other._ptr;
+            _count = other._count;
+            if ( _count != nullptr)
+            {
+                (*_count)++;
+            }
+        }
+        return *this;
+    }
 
-    ~SharedPointer();
+    ~SharedPointer()
+    {
+        if ( _ptr != nullptr )
+        {
+            (*_count)--;
+            if ( *_count == 0 )
+            {
+                delete _count;
+                _count = nullptr;
+                delete _ptr;
+                _ptr = nullptr;
+            }
+        }
+    }
 
     T* operator->() const
+    {
+        return _ptr;
+    }
 
-    T* data(); const
+    T* data() const
+    {
+        return _ptr;
+    }
 
 private:
     T* _ptr;
