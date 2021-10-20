32a3571b312313fb6fd6aeb51021961b330901f7
juangpc <juangpc@gmail.com>
2021-10-19 21:29:24 -0400

    rename membar vars and promote private class

--- src/neuromag/commandwatcher.hpp
+++ src/neuromag/commandwatcher.hpp
@@ -1,72 +1,62 @@
 //commandwatcher.hpp
-#ifndef COMMAND_WATCHER
-#define COMMAND_WATCHER
+#ifndef COMMANDWATCHER
+#define COMMANDWATCHER
 
 #include "collector_info.hpp"
-// #include "callback.hpp"
 #include "../utils/thread.hpp"
 #include "../utils/tcpsocket.hpp"
+#include "stringcallbackpair.h"
 
 #include <string>
 #include <vector>
 
 namespace Neuromag{
 
-void* watchCommands(void*);
+    void* watchCommands(void*);
 
-class CommandWatcher
-{
-public:
-    friend void* watchCommands(void*);
-    // typedef Callback<std::string, void (*)(std::string)> CommandCallback;
-
-    enum state{
-        DISCONNECTED_NOT_WATCHING,
-        CONNECTED_NOT_WATCHING,
-        CONNECTED_WATCHING
-    };
-
-    CommandWatcher();
-
-    void connect();
-
-    void connect(int, std::string);
-
-    void disconnect();
-
-    void registerCallback(std::string, void (*func)(void*), void* call);
-
-    void deleteCallback(std::string, void (*func)(void*), void* call);
-
-    void showCallbacks();
-
-    void startWatching();
-
-    void stopWatching();
-
-    state getState();
-
-    void checkForCallbacks(std::string);
-
-private:
-    struct stringCallbackPair
+    class CommandWatcher
     {
-        stringCallbackPair(std::string s, void(*fcn)(void*), void* call){trigger_string = s;callback = fcn;objPtr = call;};
-        std::string trigger_string;
-        void(* callback)(void*);
-        void* objPtr;
-        bool operator==(const stringCallbackPair& other)
-        {return (trigger_string == other.trigger_string) && (callback == other.callback);}
+    public:
+        friend void* watchCommands(void*);
+
+        enum state{
+            DISCONNECTED_NOT_WATCHING,
+            CONNECTED_NOT_WATCHING,
+            CONNECTED_WATCHING
+        };
+
+        CommandWatcher();
+
+        void connect();
+
+        void connect(int, std::string);
+
+        void disconnect();
+
+        void registerCallback(std::string, void (*func)(void*), void* call);
+
+        void deleteCallback(std::string, void (*func)(void*), void* call);
+
+        void showCallbacks();
+
+        void startWatching();
+
+        void stopWatching();
+
+        state getState();
+
+        void checkForCallbacks(std::string);
+
+    private:
+
+        std::vector<StringCallbackPair> mCallbacks;
+
+        state mState;
+
+        Thread mThread;
+
+        TCPSocket mSocket;
     };
-
-    std::vector<stringCallbackPair> m_callbacks;
-
-    state m_state;
-
-    Thread m_thread;
-
-    TCPSocket m_socket;
-};
 
 }//namespace
 #endif // COMMANDWATCHER
