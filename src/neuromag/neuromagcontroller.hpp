aa037ab1d8e08a2feeecf341ad91eed43113fc16
juangpc <juangpc@gmail.com>
2021-10-21 01:16:32 -0400

    add a global neuromagcontroller class

---
+++ src/neuromag/neuromagcontroller.hpp
@@ -0,0 +1,27 @@
+#ifndef NEUROMAG2MNE_NEUROMAGCONTROLLER_HPP
+#define NEUROMAG2MNE_NEUROMAGCONTROLLER_HPP
+
+#include "../utils/scopedpointer.hpp"
+#include "commandwatcher.hpp"
+#include "datawatcher.hpp"
+
+class NeuromagController
+{
+public:
+  NeuromagController();
+  void start();
+private:
+
+  void configureCommandWatcher();
+  void configureCommandWatcherCallbacks();
+  void configureDataWatcher();
+  void configureDataWatcherCallbacks();
+
+  bool mContinueRunning;
+  bool mAcquisitionSoftwareRunning;
+  ScopedPointer<Neuromag::CommandWatcher> mCommandWatcher;
+  ScopedPointer<Neuromag::DataWatcher> mDataWatcher;
+};
+
+
+#endif //NEUROMAG2MNE_NEUROMAGCONTROLLER_HPP
