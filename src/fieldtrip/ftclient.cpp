cfb83a58cce584da5a53705c25548dd8ba7de9b6
gabrielbmotta <gabrielbenmotta@gmail.com>
2021-11-08 16:18:56 -0500

    more work of ftbuffer classes

--- src/fieldtrip/ftclient.cpp
+++ src/fieldtrip/ftclient.cpp
@@ -1,3 +1,43 @@
-#include "sourceclient.hpp"
+#include "ftclient.hpp"
 
 
+fieldtrip::Client::Client()
+{
+}
+
+void fieldtrip::Client::connect(std::string address, unsigned short port)
+{
+  mSocket.connect(address, port);
+}
+
+void fieldtrip::Client::sendHeader(BufferParameters param)
+{
+  //to do: implement
+  (void)param;
+}
+
+void fieldtrip::Client::sendHeader(BufferParameters param, std::vector<HeaderChunk> chunkList)
+{
+  sendHeader(param);
+  for(size_t i = 0; i < chunkList.size(); ++i )
+  {
+    sendHeaderChunk(chunkList[i]);
+  }
+}
+
+void fieldtrip::Client::sendData(SharedPointer<Data> data)
+{
+  //to do: implement
+  (void)data;
+}
+
+bool fieldtrip::Client::isConnected() const
+{
+  return mSocket.isConnected();
+}
+
+void fieldtrip::Client::sendHeaderChunk(HeaderChunk chunk)
+{
+  //to do: implement
+  (void)chunk;
+}
--- src/fieldtrip/ftclient.hpp
+++ src/fieldtrip/ftclient.hpp
@@ -1,29 +1,43 @@
-#ifndef NEUROMAG2MNE_SOURCECLIENT_HPP
-#define NEUROMAG2MNE_SOURCECLIENT_HPP
+#ifndef NEUROMAG2MNE_FTCLIENT_HPP
+#define NEUROMAG2MNE_FTCLIENT_HPP
 
 #include <string>
-#include <list>
+#include <vector>
+
+#include "../utils/tcpsocket.hpp"
+#include "../utils/data.hpp"
+#include "../utils/sharedpointer.hpp"
 
 namespace fieldtrip {
 
 struct HeaderChunk{
-  int temp;
+  std::string filePath;
 };
 
 struct BufferParameters{
-  int temp;
+  int nChannels;
+  float sampleFrequency;
+  int dataType;
 };
 
-class SourceClient {
+class Client {
+public:
+  Client();
 
-  SourceClient();
-
-  void connect(std::string address, int port);
+  void connect(std::string address, unsigned short port);
   void sendHeader(BufferParameters param);
-  void sendHeader(BufferParameters param, std::list<HeaderChunk> chunkList);
+  void sendHeader(BufferParameters param, std::vector<HeaderChunk> chunkList);
 
+  void sendData(SharedPointer<Data> data);
+
+  bool isConnected() const;
+
+private:
+  void sendHeaderChunk(HeaderChunk chunk);
+
+  TCPSocket mSocket;
 };
 
 }//namespace
 
-#endif //NEUROMAG2MNE_SOURCECLIENT_HPP
+#endif //NEUROMAG2MNE_FTCLIENT_HPP
