f50e6f9803ec77c9044560ec88cd05641247db07
gabrielbmotta <gabrielbenmotta@gmail.com>
2021-11-12 15:55:26 -0500

    fstream takes cstring pre c++11

--- src/utils/fileutils.cpp
+++ src/utils/fileutils.cpp
@@ -4,7 +4,7 @@
 
 long int FileUtils::size(const std::string& filePath)
 {
-  std::ifstream file(filePath, std::ios::binary);
+  std::ifstream file(filePath.c_str(), std::ios::binary);
   file.seekg(0,std::ios::end);
   return static_cast<long int>(file.tellg());
 }
