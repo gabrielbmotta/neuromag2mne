be3eca6e88d1a4d54b33fd65bb9b0152f8029c74
gabrielbmotta <gabrielbenmotta@gmail.com>
2021-11-12 15:52:34 -0500

    formatting and fix errors

--- src/utils/fileutils.cpp
+++ src/utils/fileutils.cpp
@@ -1,12 +1,8 @@
-//
-// Created by Gabriel Motta on 11/12/21.
-//
-
 #include "fileutils.hpp"
 
 #include <fstream>
 
-long int FileUtils::qsize(const std::string& filePath)
+long int FileUtils::size(const std::string& filePath)
 {
   std::ifstream file(filePath, std::ios::binary);
   file.seekg(0,std::ios::end);
